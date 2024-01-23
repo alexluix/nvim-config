236ae822d00560934b2e065c03ecc00f83b56327
Alx Lui <alxlui@skiff.com>
2024-01-23 13:12:44 +0400

    enable LS code lens

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -10,6 +10,7 @@ local on_attach = function(_, _)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
   vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+  vim.keymap.set('n', '<c-j>', vim.lsp.codelens.run(), {})
 end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
@@ -19,10 +20,19 @@ require("lspconfig").lua_ls.setup {
   capabilities = capabilities
 }
 
-require("elixir").setup({
+local elixir = require("elixir")
+local elixirls = require("elixir.elixirls")
+
+elixir.setup({
   on_attach = on_attach,
   capabilities = capabilities,
   nextls = {enable = false},
   credo = {enable = true},
-  elixirls = {enable = true},
+  elixirls = {
+    enable = true,
+    settings = elixirls.settings {
+      enableTestLenses = true
+    }
+  },
 })
+
